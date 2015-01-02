357182ab4da10936016c8f3778b9aef45801820e
Kim Burgestrand <kim@burgestrand.se>
2015-01-02 02:40:31 +0100

    Make backtraces more descriptive.

--- lib/to_enum/source.rb
+++ lib/to_enum/source.rb
@@ -4,8 +4,10 @@ module ToEnum
   module_function
 
   def to_enum(target = self, method)
+    trace = caller_locations(1, 1)[0]
+
     target.prepend Module.new {
-      class_eval <<-CODE, __FILE__, __LINE__
+      class_eval <<-CODE.gsub("\n", ";"), trace.path, trace.lineno
         def #{method}(*args)
           if block_given?
             super
